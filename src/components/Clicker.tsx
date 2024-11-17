e2484165b1d98033bf094702cbe03e95b9bbb0a4
Daniil211212 <daniilrodionov621@gmail.com>
2024-11-17 13:18:08 +0300

    new version

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,10 +1,14 @@
 import styles from '../styles/clicker.module.css'
+let count:number = 0;
 
 const Clicker = () => {
     return(
         <div className={styles}>
             <div className={styles.bord}>
-                s
+                <div className={styles.count}>
+                    <p>Count</p>
+                    <p>{count}</p>
+                </div>
             </div>
         </div>
     )
