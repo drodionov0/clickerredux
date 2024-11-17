f74512b5831c613ac20ce3b402e95baef660ba4b
Daniil211212 <daniilrodionov621@gmail.com>
2024-11-17 13:14:39 +0300

    new version

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/clicker.module.css'
 const Clicker = () => {
     return(
         <div className={styles}>
-            s
+            <div className={styles.bord}>
+                s
+            </div>
         </div>
     )
 }
