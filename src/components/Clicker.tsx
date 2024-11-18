39a2d56533b676f1d53781bb004c17f303b925fa
Daniil211212 <daniilrodionov621@gmail.com>
2024-11-18 17:06:27 +0300

    new version

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import styles from '../styles/clicker.module.css'
-let count:number = 0;
 
 const Clicker = () => {
+    const [count, setCount] = useState<number>(0)
+
+    const handeMin = () => {setCount(count - 1)}
+    const handePlus = () => {setCount(count + 1)}
+
     return(
         <div className={styles}>
             <div className={styles.bord}>
                 <div className={styles.count}>
                     <h1>Count</h1>
                     <h2>{count}</h2>
-                    <button>+</button>
-                    <button>-</button>
+                    <button onClick={handePlus}>+</button>
+                    <button onClick={handeMin}>-</button>
                 </div>
             </div>
         </div>
