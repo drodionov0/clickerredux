1d1e227818ae0a85cdd37df26193b8cd18169a5d
Daniil211212 <daniilrodionov621@gmail.com>
2024-11-19 10:38:54 +0300

    new version

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from '../styles/clicker.module.css'
+import { increment, decrement } from '../action/CounterAction';
 
 const Clicker = () => {
-    const [count, setCount] = useState<number>(0)
-
-    const handeMin = () => {setCount(count - 1)}
-    const handePlus = () => {setCount(count + 1)}
+    const dispatch = useDispatch()
+    const count:any = useSelector((state) => state);
 
     return(
         <div className={styles}>
             <div className={styles.bord}>
                 <div className={styles.count}>
                     <h1>Count</h1>
-                    <h2>{count}</h2>
-                    <button onClick={handePlus}>+</button>
-                    <button onClick={handeMin}>-</button>
+                    <div className={styles.center}>
+                        <h2>{count}</h2>
+                    </div>
+
+                    <div className={styles.center}>
+                        <button onClick={() => dispatch(increment())} className={styles.buttonstyle}>+</button>
+                        <button onClick={() => dispatch(decrement())} className={styles.buttonstyle}>-</button>
+                    </div>
                 </div>
             </div>
         </div>
